a8acaa8791aac985ec9d5025f7412aaceed2d0b9
jalemy <jalemy.xps@gmail.com>
2025-04-23 18:05:47 +0900

    test: add mock_stream_client

---
+++ test/mock_stream_client.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:http/http.dart';
+
+class MockStreamClient implements Client {
+  final StreamController<List<int>> controller;
+  final int statusCode;
+
+  MockStreamClient({
+    required this.controller,
+    required this.statusCode,
+  });
+
+  @override
+  Future<StreamedResponse> send(BaseRequest request) {
+    return Future.value(StreamedResponse(controller.stream, statusCode));
+  }
+
+  // --- 以下、テストでは利用しないが実装が必要なメソッドたち ---
+
+  @override
+  void close() {}
+
+  @override
+  Future<Response> delete(Uri url,
+      {Map<String, String>? headers, Object? body, Encoding? encoding}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Response> get(Uri url, {Map<String, String>? headers}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Response> head(Uri url, {Map<String, String>? headers}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Response> patch(Uri url,
+      {Map<String, String>? headers, Object? body, Encoding? encoding}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Response> post(Uri url,
+      {Map<String, String>? headers, Object? body, Encoding? encoding}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Response> put(Uri url,
+      {Map<String, String>? headers, Object? body, Encoding? encoding}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> read(Uri url, {Map<String, String>? headers}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Uint8List> readBytes(Uri url, {Map<String, String>? headers}) {
+    throw UnimplementedError();
+  }
+}
